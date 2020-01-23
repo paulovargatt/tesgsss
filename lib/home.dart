1c89e15c5d9a1b380559767b47e66c260d72b001
Paulo Vargatt <pvargatt@gmail.com>
2020-01-23 16:27:55 -0300

    frirst

---
+++ lib/home.dart
@@ -0,0 +1,318 @@
+import 'package:flutter/material.dart';
+import 'package:audio_service/audio_service.dart';
+import 'package:just_audio/just_audio.dart';
+import 'dart:async';
+
+
+MediaControl playControl = MediaControl(
+  androidIcon: 'drawable/ic_action_play_arrow',
+  label: 'Play',
+  action: MediaAction.play,
+);
+MediaControl pauseControl = MediaControl(
+  androidIcon: 'drawable/ic_action_pause',
+  label: 'Pause',
+  action: MediaAction.pause,
+);
+MediaControl skipToNextControl = MediaControl(
+  androidIcon: 'drawable/ic_action_skip_next',
+  label: 'Next',
+  action: MediaAction.skipToNext,
+);
+MediaControl skipToPreviousControl = MediaControl(
+  androidIcon: 'drawable/ic_action_skip_previous',
+  label: 'Previous',
+  action: MediaAction.skipToPrevious,
+);
+MediaControl stopControl = MediaControl(
+  androidIcon: 'drawable/ic_action_stop',
+  label: 'Stop',
+  action: MediaAction.stop,
+);
+
+
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();  
+}
+
+
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+ 
+  void _audioPlayerTaskEntrypoint() async {
+    AudioServiceBackground.run(() => AudioPlayerTask());
+  }
+
+  @override
+    void initState() {
+      super.initState();
+      WidgetsBinding.instance.addObserver(this);
+      print('init');
+      connect();
+    }
+
+    @override
+    void dispose() {
+      disconnect();
+      WidgetsBinding.instance.removeObserver(this);
+      print('remove dispose');
+      super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        print('enter do resume mode');
+        connect();
+        break;
+      case AppLifecycleState.paused:
+      print('enter do paused mode');
+        disconnect();
+        break;
+      default:
+        break;
+    }
+  }
+
+  void connect() async {
+    await AudioService.connect();
+  }
+
+  void disconnect() {
+    AudioService.disconnect();
+  }
+
+  @override
+  Widget build(BuildContext context) {   
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('asd'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             RaisedButton(
+               onPressed: () {
+                
+                  _audioPlayerTaskEntrypoint;
+                 AudioService.start(
+                  backgroundTaskEntrypoint: _audioPlayerTaskEntrypoint,
+                  resumeOnClick: true,
+                  androidNotificationChannelName: 'Audio Service Demo',
+                  notificationColor: 0xFF2196f3,
+                  androidNotificationIcon: 'mipmap/ic_launcher',
+                );
+                 AudioService.play();
+              
+
+               },
+               child: Text('vargatt 21'),
+             )
+          ],
+        ),
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
+class AudioPlayerTask extends BackgroundAudioTask {
+  final _queue = <MediaItem>[
+    MediaItem(
+      id: "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3",
+      album: "1",
+      title: "1",
+      artist: "111",
+      duration: 5739820,
+      artUri:"https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
+    ),
+    MediaItem(
+      id: "https://player.vimeo.com/external/297369952.m3u8?s=c4c8a5e8819dffdd7f784bf5c91a2d4dbbf80947",
+      album: "222",
+      title: "222",
+      artist: "2",
+      duration: 5739820,
+      artUri:"https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
+    ),
+    MediaItem(
+      id: "https://s3.amazonaws.com/scifri-segments/scifri201711241.mp3",
+      album: "Science Friday",
+      title: "From Cat Rheology To Operatic Incompetence",
+      artist: "Science Friday and WNYC Studios",
+      duration: 2856950,
+      artUri: "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
+    ),
+  ];
+  int _queueIndex = -1;
+  AudioPlayer _audioPlayer = new AudioPlayer();
+  Completer _completer = Completer();
+  BasicPlaybackState _skipState;
+  bool _playing;
+
+  bool get hasNext => _queueIndex + 1 < _queue.length;
+
+  bool get hasPrevious => _queueIndex > 0;
+
+  MediaItem get mediaItem => _queue[_queueIndex];
+
+  BasicPlaybackState _stateToBasicState(AudioPlaybackState state) {
+    switch (state) {
+      case AudioPlaybackState.none:
+        return BasicPlaybackState.none;
+      case AudioPlaybackState.stopped:
+        return BasicPlaybackState.stopped;
+      case AudioPlaybackState.paused:
+        return BasicPlaybackState.paused;
+      case AudioPlaybackState.playing:
+        return BasicPlaybackState.playing;
+      case AudioPlaybackState.buffering:
+        return BasicPlaybackState.buffering;
+      case AudioPlaybackState.connecting:
+        return _skipState ?? BasicPlaybackState.connecting;
+      case AudioPlaybackState.completed:
+        return BasicPlaybackState.stopped;
+      default:
+        throw Exception("Illegal state");
+    }
+  }
+
+  @override
+  Future<void> onStart() async {
+    print('on start');
+    var playerStateSubscription = _audioPlayer.playbackStateStream
+        .where((state) => state == AudioPlaybackState.completed)
+        .listen((state) {
+      _handlePlaybackCompleted();
+    });
+    var eventSubscription = _audioPlayer.playbackEventStream.listen((event) {
+      final state = _stateToBasicState(event.state);
+      if (state != BasicPlaybackState.stopped) {
+        _setState(
+          state: state,
+          position: event.position.inMilliseconds,
+        );
+      }
+    });
+
+    AudioServiceBackground.setQueue(_queue);
+    await onSkipToNext();
+    await _completer.future;
+    playerStateSubscription.cancel();
+    eventSubscription.cancel();
+  }
+
+  void _handlePlaybackCompleted() {
+    if (hasNext) {
+      onSkipToNext();
+    } else {
+      onStop();
+    }
+  }
+
+  void playPause() {
+    if (AudioServiceBackground.state.basicState == BasicPlaybackState.playing)
+      onPause();
+    else
+      onPlay();
+  }
+
+  @override
+  Future<void> onSkipToNext() => _skip(1);
+
+  @override
+  Future<void> onSkipToPrevious() => _skip(-1);
+
+  Future<void> _skip(int offset) async {
+    final newPos = _queueIndex + offset;
+    if (!(newPos >= 0 && newPos < _queue.length)) return;
+    if (_playing == null) {
+      // First time, we want to start playing
+      _playing = true;
+    } else if (_playing) {
+      // Stop current item
+      await _audioPlayer.stop();
+    }
+    // Load next item
+    _queueIndex = newPos;
+    AudioServiceBackground.setMediaItem(mediaItem);
+    _skipState = offset > 0
+        ? BasicPlaybackState.skippingToNext
+        : BasicPlaybackState.skippingToPrevious;
+    await _audioPlayer.setUrl(mediaItem.id);
+    _skipState = null;
+    // Resume playback if we were playing
+    if (_playing) {
+      onPlay();
+    } else {
+      _setState(state: BasicPlaybackState.paused);
+    }
+  }
+
+  @override
+  void onPlay() {
+    if (_skipState == null) {
+      _playing = true;
+      _audioPlayer.play();
+    }
+  }
+
+  @override
+  void onPause() {
+    if (_skipState == null) {
+      _playing = false;
+      _audioPlayer.pause();
+    }
+  }
+
+  @override
+  void onSeekTo(int position) {
+    _audioPlayer.seek(Duration(milliseconds: position));
+  }
+
+  @override
+  void onClick(MediaButton button) {
+    playPause();
+  }
+
+  @override
+  void onStop() {
+    _audioPlayer.stop();
+    _setState(state: BasicPlaybackState.stopped);
+    _completer.complete();
+  }
+
+  void _setState({@required BasicPlaybackState state, int position}) {
+    if (position == null) {
+      position = _audioPlayer.playbackEvent.position.inMilliseconds;
+    }
+    AudioServiceBackground.setState(
+      controls: getControls(state),
+      systemActions: [MediaAction.seekTo],
+      basicState: state,
+      position: position,
+    );
+  }
+
+  List<MediaControl> getControls(BasicPlaybackState state) {
+    if (_playing) {
+      return [
+        skipToPreviousControl,
+        pauseControl,
+        stopControl,
+        skipToNextControl
+      ];
+    } else {
+      return [
+        skipToPreviousControl,
+        playControl,
+        stopControl,
+        skipToNextControl
+      ];
+    }
+  }
+}
